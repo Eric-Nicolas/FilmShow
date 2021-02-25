242dc02ccc95215e869b591bb8362d5c8f7370e9
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-02-25 10:43:32 +0100

    Made the script at level 1.

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ __author__ = 'Eric-Nicolas'
 
 
 def main() -> None:
-    print("Hello World!")
+    films = ["Voyage au centre du html", "Les 9 jsons cachés", "Algobox, le Film"]
+
+    print("Welcome to the cinema, here are the movies on the bill")
+    for i in range(len(films)):
+        print("Film {0}. {1} - Room {0}".format(i+1, films[i]))
 
 
 if __name__ == '__main__':
